8e3b5f5ab5ee975fc6e17b243e4b8b530ffbfc06
dmitry_vargin <keoelk802>
2022-06-06 20:43:04 +0300

    first compare

--- app/handlers/bids/answer_bid.py
+++ app/handlers/bids/answer_bid.py
@@ -1,3 +1,4 @@
+import requests
 from flask import request
 
 from app import app
@@ -8,19 +9,23 @@ from utils.validation import validation_request
 
 schema = {
     'status': {'required': True, 'allowed': [BidStatus.ACCEPTED.value, BidStatus.REJECTED.value]},
-    'bid_id': {'required': True, 'type': 'string'}
+    'bid_id': {'required': True, 'type': 'string'},
+    'answer': {'reqired': False, 'type': 'string', 'maxlength': 2048}
 }
 
 
-@app.route('/api/<api_version>/bids/set_bid_status', methods=['POST'])
+@app.route('/api/<api_version>/bids/answer_bid', methods=['POST'])
 @validation_request(with_token=True, schema=schema)
-def set_bid_status(api_version):
+def answer_bid(api_version):
     request_body: dict = request.json
     payload = UserModel.decode_token(request.headers['Authorization'])
     bid = BidModel.get_from_db(request_body['bid_id'])
     if not bid or bid.to_id != payload['id']:
         return ErrorManager.get_res(ErrorEnum.NOT_FOUND, f"User has not this bid")
-    upd_res = bid.update({'status': request_body.get('status')})
+    upd_res = bid.update({
+        'status': request_body.get('status'),
+        'answer': requests.get('answer', ''),
+    })
     if 'error' not in upd_res:
         return {'msg': 'ok', 'bid': upd_res}, 200
     else:
